4b6532f82ea3d760f635df5562915d16d500e444
Jussi Pakkanen <jpakkane@gmail.com>
2019-09-02 00:36:26 +0300

    Make Python 2 tests optional(er) to prepare for py2 purge in Debian.

--- mesonbuild/modules/python.py
+++ mesonbuild/modules/python.py
@@ -123,7 +123,6 @@ class PythonDependency(ExternalDependency):
                     self._find_libpy_windows(environment)
                 else:
                     self._find_libpy(python_holder, environment)
-
                 if self.is_found:
                     mlog.debug('Found "python-{}" via SYSCONFIG module'.format(self.version))
                     py_lookup_method = 'sysconfig'
@@ -131,7 +130,7 @@ class PythonDependency(ExternalDependency):
         if self.is_found:
             mlog.log('Dependency', mlog.bold(self.name), 'found:', mlog.green('YES ({})'.format(py_lookup_method)))
         else:
-            mlog.log('Dependency', mlog.bold(self.name), 'found:', [mlog.red('NO')])
+            mlog.log('Dependency', mlog.bold(self.name), 'found:', mlog.red('NO'))
 
     def _find_libpy(self, python_holder, environment):
         if python_holder.is_pypy:
@@ -153,7 +152,7 @@ class PythonDependency(ExternalDependency):
         if largs is not None:
             self.link_args = largs
 
-        self.is_found = largs is not None or not self.link_libpython
+        self.is_found = largs is not None or self.link_libpython
 
         inc_paths = mesonlib.OrderedSet([
             self.variables.get('INCLUDEPY'),
